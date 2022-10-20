78e2ec62a514f67a9f8d387d35c6bc14b6e6e317
Trainer PNT <trainer@piit.local>
2022-10-20 17:14:14 +0600

    function ,class, obj

---
+++ Day5/main.js
@@ -0,0 +1,62 @@
+
+// var name=a=>a*a*a;
+
+// console.log(name(109))
+
+
+// var obj={
+//     name:'rakib',
+//     lastName:function(){
+//         return this.name;
+//     },
+//     second:()=>this.name
+// }
+
+
+// console.log(obj.lastName())
+// console.log(obj.second())
+
+
+
+/////////////////////////////////////////////////////
+// class myClass{
+    
+//     constructor(){
+//         console.log("Constructor called");
+//     }
+
+//     getName(){
+//         return "Rakib Hassan";
+//     }
+
+//     // setName(){
+//     //     this.name=name;
+//     // }
+// }
+
+// var cls=new myClass();
+// console.log(cls.getName())
+
+// var obj={
+//     name:'rakib',
+//     lastNAme:function(){
+//         console('test')
+//     }
+// }
+
+
+// async await
+
+
+// try {
+//     var obj={
+//         name:'rakib',
+//         lastName:function(){
+//             return this.name;
+//         },
+//         second:()=>this.name
+//     }
+//     console.log(obj)
+// } catch (error) {
+//     console.log(error)
+// }
